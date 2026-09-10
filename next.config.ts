a8102c94b8411b0d3a312a958c4e9000ac1c3dad
rajabwans <wanandarajab@gmail.com>
2026-09-10 19:51:04 +0300

    Remove turbopack.root hardcoded path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: {
-    root: "/home/wanandarajab/wans-inventory-next",
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
